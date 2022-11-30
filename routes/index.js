e4e1d4e5410c88802eed5fdf390517762c5e0157
Dzhafer Khalakh <113509460+Djafer06@users.noreply.github.com>
2022-11-30 19:12:46 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,17 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Страница Карамельки */
+/* Страница Сиджея */
 router.get('/CJ', function(req, res, next) {
   res.send("<h1>CJ page</h1>")
 });
 
-/* Страница Карамельки */
+/* Страница Свита */
 router.get('/Sweet', function(req, res, next) {
   res.send("<h1>Sweet page</h1>")
 });
 
-/* Страница Карамельки */
+/* Страница БигСмоука */
 router.get('/BigSmoke', function(req, res, next) {
   res.send("<h1>BigSmoke page</h1>")
 });
