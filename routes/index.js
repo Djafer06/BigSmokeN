c4a711bd1df415c003a2d6a726fe03f63f890c15
Djafer06 <huferdzufer@gmail.com>
2022-11-30 17:19:46 +0300

    BigSmokeN

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница Карамельки */
+router.get('/CJ', function(req, res, next) {
+  res.send("<h1>CJ page</h1>")
+});
+
+/* Страница Карамельки */
+router.get('/Sweet', function(req, res, next) {
+  res.send("<h1>Sweet page</h1>")
+});
+
+/* Страница Карамельки */
+router.get('/BigSmoke', function(req, res, next) {
+  res.send("<h1>BigSmoke page</h1>")
+});
+
+
+
 module.exports = router;
